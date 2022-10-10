2eb4374c2ac4f74231051776cac9a470bc07952b
Luis Toro <luis.toro.flores97@gmail.com>
2022-10-10 01:50:23 -0400

    input coordinates and change matrix accordingly

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const readline = require('node:readline/promises');
-//const { stdin: input, stdout: output } = require('node:process');
 app.get('/', (req, res) => {
     res.sendFile('index.html', { root: __dirname });
 });
@@ -70,7 +69,7 @@ function createBoard() {
 
 function isEmptyHorizontalSpace(x, y, ship, board) {
     for (let index = 0; index < ship.size; index++) {
-        if (board[x + index][y] == []) {
+        if ( board[x][y + index] === [Object]) {
             return false
         }
     }
@@ -78,7 +77,6 @@ function isEmptyHorizontalSpace(x, y, ship, board) {
 }
 
 async function placeShip(ship, board) {
-    console.log(ship.name);
     var rl = readline.createInterface(process.stdin, process.stdout);
 
     let positionX = await rl.question(`Position in X for: , ${ship.name}: `, (posx) => {
@@ -92,24 +90,47 @@ async function placeShip(ship, board) {
         for (let i = 0; i < ship.size; i++) {
             board[parseInt(positionX)][parseInt(positionY) + i] = ship
         }
+    }else{
+        console.log("no room");
     }
 }
 
-function shoot(x, y, board) {
-    console.log(destructorCounter);
-    if (!board[x][y] == []) {
-        obj = board[x][y]
-        typeOfShip = obj.name
-        reduceShipLife(typeOfShip);
-        board[x][y] = "X"
-        console.log(board);
-    } else {
-        board[x][y] = "O"
+function isCoordinateEmpty(x,y,board){
+    console.log(board[x][y]);
+    if(board[x][y].length === [].length){
+        return true
+    }else{
+        return false
     }
-    //cambiar de turno y verificar si se gano
+}
+//parece que no esta cerrando bien las interfaces de pregunta y se acumulan los listeners averiguar para cerrarlos y reduceshiplife no corre
+
+async function shoot(board) {
+    while (checkForWins()) {
+        var rl = readline.createInterface(process.stdin, process.stdout);
+
+        let shootX = await rl.question(`Where do you want to shoot for Position in X: `, (posx) => {
+            rl.close();
+        });
+        let shootY = await rl.question(`Where do you want to shoot for Position in Y: `, (posy) => {
+            rl.close();
+        });
+        if (board[parseInt(shootX)][parseInt(shootY)].length != [].length) {
+            let ship = board[parseInt(shootX)][parseInt(shootY)]
+            let typeOfShip = ship.name
+            reduceShipLife(typeOfShip);
+            board[parseInt(shootX)][parseInt(shootY)] = "X"
+            console.clear()
+            console.table(board);
+        } else {
+            board[parseInt(shootX)][parseInt(shootY)] = "O"
+        }
+    } 
+    return 0;
 }
 
 function reduceShipLife(typeOfShip) {
+    console.log(typeOfShip);
     if (typeOfShip === 'destructor') destructorCounter--
     if (typeOfShip === 'submarine') submarineCounter--
     if (typeOfShip === 'cruiser') cruiserCounter--
@@ -120,11 +141,13 @@ function reduceShipLife(typeOfShip) {
 async function checkForWins() {
     if ((destructorCounter + submarineCounter + cruiserCounter + battleshipCounter + carrierCounter) === 0) {
         console.log("player 1 wins");
+        return false
     }
     if ((cpuDestructorCounter + cpuSubmarineCounter + cpuCruiserCounter + cpuBattleshipCounter + cpuCarrierCounter) === 0) {
         console.log("player 2 wins");
+        return false
     }
-    console.log("sqqsq");
+    return true
 }
 
 var schema = {
@@ -150,8 +173,12 @@ async function execute() {
     board1 = createBoard();
     console.table(board1);
     //await placeShip(shipArray[0],board1)
+    //await placeShip(shipArray[0],board1)
     await populateMatrix(board1);
+    console.clear()
     console.table(board1);
+    //console.log(isCoordinateEmpty(0,0,board1)); 
+    shoot(board1);
 }
 execute()
 
